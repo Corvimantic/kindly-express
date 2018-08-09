967fcddd415b3d32c6414bba3fb080f49ab68db4
Hao-Han Pan <c.avantin94@gmail.com>
2018-08-08 20:36:17 -0500

    Purge irrelevant files from starter kit

--- server/index.js
+++ server/index.js
@@ -7,16 +7,6 @@ var app = express();
 app.use(express.static(__dirname + '/../client'));
 app.use(express.static(__dirname + '/../node_modules'));
 
-app.get('/items', function (req, res) {
-  items.selectAll(function(err, data) {
-    if(err) {
-      res.sendStatus(500);
-    } else {
-      res.json(data);
-    }
-  });
-});
-
 app.listen(3000, function() {
   console.log('listening on port 3000!');
 });
