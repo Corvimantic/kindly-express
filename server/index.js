ef3fad20731924432618dfb9492a88e970f8866d
Hao-Han Pan <c.avantin94@gmail.com>
2018-08-11 03:55:46 -0500

    Stub out server to work with tempStorage

--- server/index.js
+++ server/index.js
@@ -3,6 +3,9 @@ var bodyParser = require('body-parser');
 //var items = require('../database');
 
 var app = express();
+var textParser = bodyParser.text();
+
+var tempStorage = [];
 
 app.use(express.static(__dirname + '/../client'));
 app.use(express.static(__dirname + '/../node_modules'));
@@ -11,12 +14,16 @@ app.get('/submit', function (req, res) {
   res.redirect('/#!submit');
 })
 
-app.post('/submit', function (req, res) {
+app.post('/submit', textParser, function (req, res) {
   // TODO
+  console.log(req.body);
+  tempStorage.push(req.body);
+  res.send(true);
 })
 
 app.get('/messages', function (req, res) {
   // TODO
+  res.send(tempStorage[0]);
 })
 
 app.listen(3000, function() {
