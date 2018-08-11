be48b00a9b7cccec536772de1b197783410777d1
Hao-Han Pan <c.avantin94@gmail.com>
2018-08-11 03:22:28 -0500

    Fill out frontend controllers

--- client/components/home.js
+++ client/components/home.js
@@ -1,5 +1,13 @@
 angular.module('app')
 .component('home', {
-  controller: function() {},
+  controller: function() {
+    this.message = 'hi';
+    this.$onInit = function() {
+      console.log('hello');
+      // GET /messages
+      // set message to result
+    };
+    this.$onInit.bind(this);
+  },
   templateUrl: '/templates/home.html'
 });
--- client/components/submit.js
+++ client/components/submit.js
@@ -1,5 +1,14 @@
 angular.module('app')
 .component('submit', {
-  controller: function() {},
+  controller: function() {
+    this.submission = '';
+    this.submitted = false;
+    this.submit = function(message) {
+      console.log(this.submission);
+      this.submission = '';
+      this.submitted = true;
+    };
+    this.submit.bind(this);
+  },
   templateUrl: '/templates/submit.html'
 });
