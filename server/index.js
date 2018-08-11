affd0c49b5453370551d0d716d55c832ae3ee3aa
Hao-Han Pan <c.avantin94@gmail.com>
2018-08-10 23:11:28 -0500

    Add redirect route for /submit GET

--- server/index.js
+++ server/index.js
@@ -7,6 +7,10 @@ var app = express();
 app.use(express.static(__dirname + '/../client'));
 app.use(express.static(__dirname + '/../node_modules'));
 
+app.get('/submit', function (req, res) {
+  res.redirect('/#!submit');
+})
+
 app.listen(3000, function() {
   console.log('listening on port 3000!');
 });
