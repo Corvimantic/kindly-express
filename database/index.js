63677a2b2440d749d28b076dff655c6bacf93df2
Hao-Han Pan <c.avantin94@gmail.com>
2018-08-12 23:34:51 -0500

    Fill out database functions

--- database/index.js
+++ database/index.js
@@ -1,21 +1,50 @@
 var mysql = require('mysql');
 var password = require('./env/config.js');
+var Promise = require('bluebird');
 
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : password,
-  database : 'test'
+  database : 'kindly'
 });
 
-var selectAll = function(callback) {
-  connection.query('SELECT * FROM items', function(err, results, fields) {
+var selectRandomMessage = function(callback) {
+  return new Promise(function(resolve, reject) {
+    connection.query('SELECT COUNT(*) FROM messages', function(err, results, fields) {
+      if (err) {
+        reject(err);
+      } else {
+        var id = getRandomInt(results);
+        resolve(id);
+      }
+    });
+  })
+  .then(function(id) {
+    connection.query(`SELECT * FROM messages WHERE id = ${id}`, function(err, results, fields) {
+      if(err) {
+        callback(err, null);
+      } else {
+        callback(null, results);
+      }
+    });
+  });
+};
+
+var saveNewMessage = function(message, callback) {
+  connection.query(`INSERT INTO messages (message) VALUE ("${message}")`, function(err, results, fields) {
     if(err) {
       callback(err, null);
     } else {
       callback(null, results);
     }
   });
-};
+}
 
-module.exports.selectAll = selectAll;
+function getRandomInt(max) {
+  max = Math.floor(max + 1);
+  return Math.floor(Math.random() * (max - 1)) + 1;
+}
+
+module.exports.selectRandomMessage = selectRandomMessage;
+module.exports.saveNewMessage = saveNewMessage;
